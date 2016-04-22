d9cf4cd6c496efe9072cc313073409914c146fda
Boris Mossounov <borix@tut.by>
2016-04-22 15:04:38 +0300

    Testing CI

--- tests/phpunit/test-sample.php
+++ tests/phpunit/test-sample.php
@@ -15,7 +15,7 @@ class SampleTest extends WP_UnitTestCase {
 	 */
 	function test_sample() {
 		// Replace this with some actual testing code.
-		$this->assertTrue( false );
+		$this->assertTrue( true );
 	}
 }
 
