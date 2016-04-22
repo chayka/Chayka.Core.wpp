b56706426373e313bbab4a363b8bcbb884c7c1bd
Boris Mossounov <borix@tut.by>
2016-04-22 16:16:48 +0300

    Testing CI

--- tests/casperjs/test.sh
+++ tests/casperjs/test.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 wpUrl=${1-${WP_CI_URL}}
+extra=""
+if [ ! -z ${PHPCI} ]; then
+    extra=" --no-color"
+fi
 
 if [ ! -z ${wpUrl} ]; then
-    casperjs test --includes=bootstrap.js --wpUrl=${wpUrl} test-*.js
+    casperjs test --includes=bootstrap.js --wpUrl=${wpUrl}${extra} test-*.js
 else
     echo "please specify WordPress instance url as param, e.g.: test.sh localhost"
 fi
