0f2fefd58d41dc4a117f8871b2f43d549c4e051a
Boris Mossounov <borix@tut.by>
2016-06-09 14:58:06 +0300

    Working on plugins update

--- app/helpers/UpdateClientHelper.php
+++ app/helpers/UpdateClientHelper.php
@@ -190,20 +190,21 @@ class UpdateClientHelper{
             $updates = self::getUpdates();
             $update = Util::getItem($updates, $slug);
             if($update){
-                $obj = new stdClass();
-                $obj->slug = $slug;
-                $obj->plugin_name = 'plugin.php';
-                $obj->new_version = '1.1';
-                $obj->requires = '3.0';
-                $obj->tested = '3.3.1';
-                $obj->downloaded = 12540;
-                $obj->last_updated = '2012-01-12';
-                $obj->sections = array(
-                    'description' => 'The new version of the Auto-Update plugin',
-                    'another_section' => 'This is another section',
-                    'changelog' => 'Some new features'
-                );
-                $obj->download_link = 'http://localhost/update.php';
+                $obj = (object) $update;
+//                $obj = new stdClass();
+//                $obj->slug = $slug;
+//                $obj->plugin_name = 'plugin.php';
+//                $obj->new_version = '1.1';
+//                $obj->requires = '3.0';
+//                $obj->tested = '3.3.1';
+//                $obj->downloaded = 12540;
+//                $obj->last_updated = '2012-01-12';
+//                $obj->sections = array(
+//                    'description' => 'The new version of the Auto-Update plugin',
+//                    'another_section' => 'This is another section',
+//                    'changelog' => 'Some new features'
+//                );
+//                $obj->download_link = 'http://localhost/update.php';
                 return $obj;
             }
         }
